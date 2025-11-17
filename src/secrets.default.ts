6a8f782b1da175f836441cb0c85675c496694ae1
van-al <alex.vanstek@gmail.com>
2025-11-17 14:14:52 -0500

    Add dotenv support and environment-based secrets
    Introduced .env.example for environment variable configuration and added dotenv as a dev dependency. Updated secrets.default.ts to read secrets from environment variables with fallbacks to defaults. Modified webpack.config.js to inject environment variables into the build process, enabling easier configuration for different environments.

--- src/secrets.default.ts
+++ src/secrets.default.ts
@@ -1,22 +1,23 @@
-export const connectHostname = "wss://connect-open.airmessage.org";
+// Read from environment variables or use defaults
+export const connectHostname = process.env.REACT_APP_CONNECT_HOSTNAME || "wss://connect-open.airmessage.org";
 
-export const googleApiKey = "AIzaSyDE2nDAKL6smwPmZIBy1IP8-x_pTOqpzfM";
-export const googleClientID = "526640769548-gv7t20cb7evjgmnngnl804gm0ec3kl8h.apps.googleusercontent.com";
-export const googleClientSecret = "AHUS5_B5Ahmas7ioaoiBact0";
+export const googleApiKey = process.env.REACT_APP_GOOGLE_API_KEY || "AIzaSyDE2nDAKL6smwPmZIBy1IP8-x_pTOqpzfM";
+export const googleClientID = process.env.REACT_APP_GOOGLE_CLIENT_ID || "526640769548-gv7t20cb7evjgmnngnl804gm0ec3kl8h.apps.googleusercontent.com";
+export const googleClientSecret = process.env.REACT_APP_GOOGLE_CLIENT_SECRET || "AHUS5_B5Ahmas7ioaoiBact0";
 
 export const firebaseConfig = {
-	apiKey: "AIzaSyDE2nDAKL6smwPmZIBy1IP8-x_pTOqpzfM",
-	authDomain: "airmessage-open.firebaseapp.com",
-	projectId: "airmessage-open",
-	storageBucket: "airmessage-open.appspot.com",
-	messagingSenderId: "526640769548",
-	appId: "1:526640769548:web:2b0b11b66424b3ce805c29"
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyDE2nDAKL6smwPmZIBy1IP8-x_pTOqpzfM",
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "airmessage-open.firebaseapp.com",
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "airmessage-open",
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "airmessage-open.appspot.com",
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "526640769548",
+	appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:526640769548:web:2b0b11b66424b3ce805c29"
 };
 
-export const sentryDSN = undefined;
+export const sentryDSN = process.env.REACT_APP_SENTRY_DSN || undefined;
 
 export const jwkLocalEncryption: JsonWebKey = {
 	kty: "oct",
-	k: "s9lDeHtl0rh-3FpBDZwQvw",
+	k: process.env.REACT_APP_JWK_LOCAL_ENCRYPTION_KEY || "s9lDeHtl0rh-3FpBDZwQvw",
 	alg: "A128GCM"
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require("webpack");
 const path = require("path");
 const fs = require("fs");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+require("dotenv").config();
 // const ESLintPlugin = require("eslint-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
@@ -104,7 +105,20 @@ module.exports = (env) => ({
 			"WPEnv.ENVIRONMENT": JSON.stringify(env.WEBPACK_SERVE ? "development" : "production"),
 			"WPEnv.PACKAGE_VERSION": JSON.stringify(process.env.npm_package_version),
 			"WPEnv.RELEASE_HASH": "\"undefined\"",
-			"WPEnv.BUILD_DATE": Date.now()
+			"WPEnv.BUILD_DATE": Date.now(),
+			// Inject environment variables
+			"process.env.REACT_APP_CONNECT_HOSTNAME": JSON.stringify(process.env.REACT_APP_CONNECT_HOSTNAME),
+			"process.env.REACT_APP_GOOGLE_API_KEY": JSON.stringify(process.env.REACT_APP_GOOGLE_API_KEY),
+			"process.env.REACT_APP_GOOGLE_CLIENT_ID": JSON.stringify(process.env.REACT_APP_GOOGLE_CLIENT_ID),
+			"process.env.REACT_APP_GOOGLE_CLIENT_SECRET": JSON.stringify(process.env.REACT_APP_GOOGLE_CLIENT_SECRET),
+			"process.env.REACT_APP_FIREBASE_API_KEY": JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
+			"process.env.REACT_APP_FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+			"process.env.REACT_APP_FIREBASE_PROJECT_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+			"process.env.REACT_APP_FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+			"process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+			"process.env.REACT_APP_FIREBASE_APP_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_APP_ID),
+			"process.env.REACT_APP_SENTRY_DSN": JSON.stringify(process.env.REACT_APP_SENTRY_DSN),
+			"process.env.REACT_APP_JWK_LOCAL_ENCRYPTION_KEY": JSON.stringify(process.env.REACT_APP_JWK_LOCAL_ENCRYPTION_KEY)
 		}),
 	].concat(!env.WEBPACK_SERVE ? new WorkboxPlugin.GenerateSW() : [])
 });
